ab8c0ba1f3b6b496080f5b921478d1531257bfd1
Nurmuhammed0 <nur26kudayarov@gmail.com>
2026-06-03 10:45:49 +0600

    new safe

--- src/Market-Nur/Menu/All.js
+++ src/Market-Nur/Menu/All.js
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
 import ShopAll from './ShopAll'
 
 export default function All(props) {
   const [add] = useState([])
-  const { id } = useParams()
   // useEffect(()=>{
 
   // })
